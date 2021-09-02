f4bf59bbeea363debfd56c6928dd0a94c7bd31ad
bmercer123 <59985570+bmercer123@users.noreply.github.com>
2021-09-03 01:31:01 +1200

    Basic Idle and running animation
    Tarnsition from 1 animation to another

--- OnTheWayHome/Assets/Player_Controller.cs
+++ OnTheWayHome/Assets/Player_Controller.cs
@@ -5,28 +5,37 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
     public Rigidbody2D rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator anim;
 
     // Update is called once per frame
     void Update()
     {
+        //Character movement (Ex. Press A key or left arrow and the character goes left )
+
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
+            // Transform so that the sprite position changes with the movement of the character
             transform.localScale = new Vector2(-1, 1);
+            anim.SetBool("running", true);
         }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+
+
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
+            anim.SetBool("running", true);
         }
+
+        else 
+        {
+            anim.SetBool("running", false); 
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 5);
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
         }
     }
 }
