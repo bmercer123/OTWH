d6eb30a5db30a2e6c0af537f343500e625fd36aa
bmercer123 <59985570+bmercer123@users.noreply.github.com>
2021-09-03 21:57:02 +1200

    Partial animation functions done for run and idle

--- OnTheWayHome/Assets/Player_Controller.cs
+++ OnTheWayHome/Assets/Player_Controller.cs
@@ -4,38 +4,68 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public Animator anim;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private enum State {idle, running, jumping};
+    private State state = State.idle;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
 
     // Update is called once per frame
     void Update()
     {
+        float hDirection = Input.GetAxis("Horizontal");
         //Character movement (Ex. Press A key or left arrow and the character goes left )
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (hDirection < 0)
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
             // Transform so that the sprite position changes with the movement of the character
             transform.localScale = new Vector2(-1, 1);
-            anim.SetBool("running", true);
         }
 
 
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        else if (hDirection > 0)
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
-            anim.SetBool("running", true);
         }
 
         else 
         {
-            anim.SetBool("running", false); 
+             
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = new Vector2(rb.velocity.x, 10f);
+            state = State.jumping;
+        }
+        VelocityState();
+        anim.SetInteger("state", (int)state);
+
+    }
+
+    private void VelocityState()
+    {
+        if (state == State.jumping)
+        {
+
+        }
+
+        else if (Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            //Moving 
+            state = State.running;
+        }
+        else 
+        {
+            state = State.idle;
         }
     }
 }
