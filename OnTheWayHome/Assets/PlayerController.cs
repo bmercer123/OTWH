cfeeff6d3e5a0270695fe0452368b835aa05967a
bmercer123 <59985570+bmercer123@users.noreply.github.com>
2021-09-08 03:07:30 +1200

    Attempting to fix issue with gitignore

---
+++ OnTheWayHome/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+
+    private void Update()
+    {
+        //Movement of the player (Ex. If A or left arrow is pressed, character moves left)
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 10);
+        }
+    }
+
+}
---
+++ OnTheWayHome/Assets/Scripts/Frog.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : Enemy
+{
+    [SerializeField]private float leftCap;
+    [SerializeField]private float rightCap;
+
+    [SerializeField] private float jumpLength = 10f;
+    [SerializeField] private float jumpHeight = 20f;
+    [SerializeField] private LayerMask ground;
+    private Collider2D coll;
+   
+    
+
+    private bool facingLeft = true;
+
+    protected override void Start()
+    {
+        base.Start();
+        coll = GetComponent<Collider2D>();
+        
+      
+    }
+
+    private void Update()
+    {
+        //Transit form Jumping to Falling
+        if (anim.GetBool("Jumping")) 
+        {
+            if (rb.velocity.y < .1)
+            {
+                anim.SetBool("Falling", true);
+                anim.SetBool("Jumping", false);
+            }
+        }
+
+        //Transit from Falling to Idle
+        {
+            if (coll.IsTouchingLayers(ground) && anim.GetBool("Falling"))
+            {
+                anim.SetBool("Falling", false);
+            }
+        }
+    }
+
+    private void eMovement()
+    {
+        if (facingLeft)
+        {
+            //Test to see if beyond leftCap
+            if (transform.position.x > leftCap)
+            {
+                //Check for sprite facing direction, if not correct, then face correct direction
+                if (transform.localScale.x != 1)
+                {
+                    transform.localScale = new Vector3(1, 1);
+                }
+                //Check if object on the ground, if so, jump
+                if (coll.IsTouchingLayers(ground))
+                {
+                    rb.velocity = new Vector2(-jumpLength, jumpHeight);
+                    anim.SetBool("Jumping", true);
+                }
+            }
+            else
+            {
+                facingLeft = false;
+            }
+        }
+        else
+        {
+            {
+                //Test to see if beyond leftCap
+                if (transform.position.x < rightCap)
+                {
+                    //Check for sprite facing direction, if not correct, then face correct direction
+                    if (transform.localScale.x != -1)
+                    {
+                        transform.localScale = new Vector3(-1, 1);
+                    }
+                    //Check if object on the ground, if so, jump
+                    if (coll.IsTouchingLayers(ground))
+                    {
+                        rb.velocity = new Vector2(jumpLength, jumpHeight);
+                        anim.SetBool("Jumping", true);
+                    }
+                }
+                else
+                {
+                    facingLeft = true;
+                }
+            }
+        }
+    }
+
+   
+}
